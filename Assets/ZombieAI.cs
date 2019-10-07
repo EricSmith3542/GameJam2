85a3387490096f230f9df62beff39bc48337d263
Eric Smith <ericsmith3542@gmail.com>
2019-10-07 12:23:50 -0700

    Zombie AI added

---
+++ Assets/ZombieAI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Animator anim;
+    public Transform player;
+    public float chaseDistance;
+    public float stunTime;
+
+    public enum AIState { idle, chase, stunned }
+
+    public AIState aiState = AIState.idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(Think());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Think()
+    {
+        while (true)
+        {
+            float dist = Vector3.Distance(player.position, transform.position);
+            switch (aiState)
+            {
+                case AIState.idle:
+                    if(dist <= chaseDistance)
+                    {
+                        aiState = AIState.chase;
+                        anim.SetBool("chase", true);
+                    }
+                    navMeshAgent.SetDestination(transform.position);
+                    break;
+                case AIState.chase:
+                    if (dist > chaseDistance)
+                    {
+                        aiState = AIState.idle;
+                        anim.SetBool("chase", false);
+                    }
+                    navMeshAgent.SetDestination(player.position);
+                    break;
+                case AIState.stunned:
+                    anim.SetBool("stunned", true);
+                    navMeshAgent.isStopped = true;
+                    yield return new WaitForSeconds(stunTime);
+                    aiState = AIState.idle;
+                    navMeshAgent.isStopped = false;
+                    anim.SetBool("stunned", false);
+                    break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
